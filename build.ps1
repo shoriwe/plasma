c8dc4c07fac6f0c609f023ef540ac10060d4b073
Antonio <antoniojosedonishung@gmail.com>
2022-06-21 17:26:32 -0500

    Build script

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$target_os = "windows", "linux";
+$target_archs = "amd64", "386";
+
+$env:GOPRIVATE="*";
+$env:GONOSUMDB="*";
+
+Remove-Item -LiteralPath "build" -Force -Recurse
+
+Foreach ($os IN $target_os) {
+    If ($os -eq "windows") {
+        $extension = ".exe";
+    } Else {
+        $extension = "";
+    }
+    Foreach ($arch IN $target_archs) {
+        $env:GOOS = $os;
+        $env:GOARCH = $arch;
+        go build -v -o "build/plasma-repl-$os-$arch$extension" -ldflags="-s -w" -trimpath -buildvcs=false -mod vendor ./cmd/plasma;
+    }
+}
