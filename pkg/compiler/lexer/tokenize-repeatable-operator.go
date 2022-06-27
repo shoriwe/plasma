0503a9fb5d361cc373d26f62ee6ed1bebce5a66d
Antonio <antoniojosedonishung@gmail.com>
2022-06-27 15:44:23 -0500

    clear implementation

--- pkg/compiler/lexer/tokenize-repeatable-operator.go
+++ pkg/compiler/lexer/tokenize-repeatable-operator.go
@@ -8,27 +8,33 @@ func (lexer *Lexer) tokenizeRepeatableOperator(
 ) {
 	lexer.currentToken.Kind = singleKind
 	lexer.currentToken.DirectValue = singleDirectValue
-	if lexer.reader.HasNext() {
-		nextChar := lexer.reader.Char()
-		if nextChar == lexer.currentToken.Contents[0] {
-			lexer.currentToken.append(nextChar)
-			lexer.reader.Next()
-			lexer.currentToken.Kind = doubleKind
-			lexer.currentToken.DirectValue = doubleDirectValue
-			if lexer.reader.HasNext() {
-				nextNextChar := lexer.reader.Char()
-				if nextNextChar == '=' {
-					lexer.currentToken.append(nextNextChar)
-					lexer.currentToken.Kind = assignDoubleKind
-					lexer.reader.Next()
-					lexer.currentToken.DirectValue = assignDoubleDirectValue
-				}
-			}
-		} else if nextChar == '=' {
-			lexer.currentToken.Kind = assignSingleKind
-			lexer.currentToken.append(nextChar)
-			lexer.reader.Next()
-			lexer.currentToken.DirectValue = assignSingleDirectValue
-		}
+	if !lexer.reader.HasNext() {
+		return
 	}
+	nextChar := lexer.reader.Char()
+	if nextChar == '=' {
+		lexer.currentToken.Kind = assignSingleKind
+		lexer.currentToken.append(nextChar)
+		lexer.reader.Next()
+		lexer.currentToken.DirectValue = assignSingleDirectValue
+		return
+	}
+	if nextChar != lexer.currentToken.Contents[0] {
+		return
+	}
+	lexer.currentToken.append(nextChar)
+	lexer.reader.Next()
+	lexer.currentToken.Kind = doubleKind
+	lexer.currentToken.DirectValue = doubleDirectValue
+	if !lexer.reader.HasNext() {
+		return
+	}
+	nextNextChar := lexer.reader.Char()
+	if nextNextChar != '=' {
+		return
+	}
+	lexer.currentToken.append(nextNextChar)
+	lexer.currentToken.Kind = assignDoubleKind
+	lexer.reader.Next()
+	lexer.currentToken.DirectValue = assignDoubleDirectValue
 }
